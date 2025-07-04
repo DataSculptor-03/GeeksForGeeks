4385b93e88460b37d29f74444a3aa6e7de41cb20
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-07-05 00:24:00 +0530

    Create Count Odd and Even.py

---
+++ Count Odd and Even.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOddEven(self, arr):
+        #Code here
+        c1=0
+        c2=0
+        for i in range(len(arr)):
+            if(arr[i]%2==0):
+                c1+=1
+            else:
+                c2+=1
+        return c2,c1
