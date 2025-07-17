782fe793a3c6ae43e158efa223902add0b58ba5c
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-07-18 01:23:16 +0530

    Create Vowel or Not.py

---
+++ Vowel or Not.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isVowel (ob,c):
+        # code here 
+        if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=='A' or c=='E' or c=='I' or c=='O' or c=='U'):
+            return "YES"
+        else:
+            return "NO"
