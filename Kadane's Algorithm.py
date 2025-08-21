ff8c1316a9edffaf72ef5a64a4f369ede489e579
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-08-21 21:54:09 +0530

    Create Kadane's Algorithm.py

---
+++ Kadane's Algorithm.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+        # Code here
+        max_sum=float('-inf')
+        current_sum=0
+        for num in arr:
+            current_sum=max(num,current_sum+num)
+            max_sum=max(max_sum,current_sum)
+        return max_sum
