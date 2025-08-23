099c4a6d2517e27dababdc931bcec31f7023878c
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-08-23 22:21:33 +0530

    Create Kth largest element.py

---
+++ Kth largest element.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+    #Function to return kth largest element from an array.
+    def kthLargest(self,arr,k):
+        
+        # code here
+        arr.sort()
+        return arr[-k]
